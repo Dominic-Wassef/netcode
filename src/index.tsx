03c6fc6ac6aae8bcb74430f05b32be74b8a44848
dominic-wassef <youturncorporation@gmail.com>
2022-04-17 14:28:46 -0400

    adding

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: './esbuild.wasm'
+        });
+        console.log(service);
+    };
+    useEffect(() => {
+        startService();
+    }, []);
 
     const onClick = () => {
         console.log(input);
