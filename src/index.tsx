c1bf909ef4fe867167ad459ef60adf7e99610ef3
dominic-wassef <youturncorporation@gmail.com>
2022-04-20 22:20:07 -0400

    adding

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: './esbuild.wasm'
         });
-        console.log(service);
     };
     useEffect(() => {
         startService();
     }, []);
 
     const onClick = () => {
-        console.log(input);
-    }
+        if (!ref.current) {
+            return;
+        }
+
+        console.log(ref.current);
+    };
 
     return ( 
     <div>
