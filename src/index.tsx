cbc3451eadc99676181a4a282a146cda581d78b0
dominic-wassef <youturncorporation@gmail.com>
2022-04-21 19:44:18 -0400

    adding

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,52 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>();
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: './esbuild.wasm'
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
-        });
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
 
-        setCode(result.code);
-    };
+    console.log(result);
 
-    return ( 
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
     <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}>
-        </textarea>
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <pre>{code}</pre>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-    );
+  );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
