f93576dd8be6412cb1cd0666b02a977085baec91
dominic-wassef <youturncorporation@gmail.com>
2022-04-26 22:40:52 -0400

    implementing and handling code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,22 +24,41 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
       }
     });
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <html>
+    <head><head>
+    <body>
+    <div id="root"></div>
+    <script>
+      window.addEventListener('message', (event) => {
+         try {
+         eval(event.data);
+        } catch (err) {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        }
+        }, false);
+      </script>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -50,7 +69,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="iframe" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -4,18 +4,17 @@ export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      
       // Handle root entry file of 'index.js'
-      build.onResolve({ filter: /(^index\.js$)/}, () => {
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return { path: 'index.js', namespace: 'a' };
       });
 
       // Handle relative paths in a module
-      build.onResolve({ filter: /^\.+\//}, (args: any) => {
-        return { 
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
           namespace: 'a',
-          path: new URL(args.path, 
-            'https://unpkg.com' + args.resolveDir + '/').href
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
         };
       });
 
@@ -23,8 +22,8 @@ export const unpkgPathPlugin = () => {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
-        }
+          path: `https://unpkg.com/${args.path}`,
+        };
       });
     },
   };
